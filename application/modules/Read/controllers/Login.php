73a5f87ed7929590ccbe57a25ef42b5acf277aeb
林锋 <lf@linfengdeMacBook-Pro.local>
2021-05-07 11:05:31 +0800

    two commit modules

---
+++ application/modules/Read/controllers/Login.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * @Author: Grove
+ * @File  : Index.php
+ * @Date  : 2021/4/25 2:54 下午
+ * @last  : Modified by: I will never know what the next difficulty is
+ */
+
+class LoginController extends Yaf_Controller_Abstract
+{
+    public function indexAction()
+    {
+        echo  'hello Login';
+        return false;
+    }
+
+}
---
+++ application/modules/Work/controllers/Index.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @Author: Grove
+ * @File  : Index.php
+ * @Date  : 2021/4/25 2:54 下午
+ * @last  : Modified by: I will never know what the next difficulty is
+ */
+
+class IndexController extends Yaf_Controller_Abstract
+{
+    public function indexAction()
+    {
+        echo  'hello linfeng';
+        return false;
+    }
+    public function testAction()
+    {
+        echo 'ok';
+        return false;
+    }
+}
